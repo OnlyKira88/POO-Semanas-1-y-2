acb6dfdda22b5731745d4ff360d0c14b8031a7e6
OnlyKira88 <meliodasxdxdxd@gmail.com>
2026-08-29 20:52:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from semana2 import Universidad, EstudianteUniversitario
 
+
+nombre = input("Nombre: ")
+universidad = input("Universidad: ")
+
+nota1 = float(input("Nota 1: "))
+nota2 = float(input("Nota 2: "))
+nota3 = float(input("Nota 3: "))
+
+
+universidad1 = Universidad(universidad)
+
+estudiante1 = EstudianteUniversitario(
+    nombre,
+    nota1,
+    nota2,
+    nota3,
+    universidad1
+)
+
+estudiante1.mostrar_informacion()
