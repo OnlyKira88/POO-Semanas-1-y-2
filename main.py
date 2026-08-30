749f0167813f61afebdc6c08ac870a078071fed6
OnlyKira88 <meliodasxdxdxd@gmail.com>
2026-08-29 21:04:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from semana2 import Universidad, EstudianteUniversitario 
+from semana2 import Universidad, EstudianteUniversitario
 
-nombre = input("nombre:")
-universidad = input("Universidad:")
+nombre = input("Nombre: ")
+universidad = input("Universidad: ")
 
-nota1 = float(input("Nota 1:"))
-nota2 = float(input("Nota 2:"))
-nota3 = float(input("Nota 3:"))
+nota1 = float(input("Nota 1 (0-10): "))
+while nota1 < 0 or nota1 > 10:
+    print("La nota debe estar entre 0 y 10.")
+    nota1 = float(input("Nota 1 (0-10): "))
+
+nota2 = float(input("Nota 2 (0-10): "))
+while nota2 < 0 or nota2 > 10:
+    print("La nota debe estar entre 0 y 10.")
+    nota2 = float(input("Nota 2 (0-10): "))
+
+nota3 = float(input("Nota 3 (0-10): "))
+while nota3 < 0 or nota3 > 10:
+    print("La nota debe estar entre 0 y 10.")
+    nota3 = float(input("Nota 3 (0-10): "))
 
 universidad1 = Universidad(universidad)
 
@@ -14,6 +25,6 @@ estudiante1 = EstudianteUniversitario(
     nota1,
     nota2,
     nota3,
-    universidad1,
+    universidad1
 )
-    estudiante1.mostrar_informacion()
+estudiante1.mostrar_informacion()
